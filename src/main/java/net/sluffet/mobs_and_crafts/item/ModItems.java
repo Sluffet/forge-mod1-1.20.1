86549bca2ccdda8f8e9cc0cd63eb81b47bc454f1
Sluffet <Levip2012@outlook.com>
2025-11-22 22:54:53 +0100

    help please

---
+++ src/main/java/net/sluffet/mobs_and_crafts/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.sluffet.mobs_and_crafts.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sluffet.mobs_and_crafts.MobsandCrafts;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MobsandCrafts.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
